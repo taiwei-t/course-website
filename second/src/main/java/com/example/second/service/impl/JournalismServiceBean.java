ad6544b1ed7babf7e84f82cc12c6f35eaed6b295
taiwei-t <taiwei-t@qq.com>
2021-06-09 08:39:02 +0800

    增加新闻的后端验证

--- second/src/main/java/com/example/second/service/impl/JournalismServiceBean.java
+++ second/src/main/java/com/example/second/service/impl/JournalismServiceBean.java
@@ -1,54 +1,54 @@
 package com.example.second.service.impl;
 
-//import com.example.second.dao.JournalismCopyDao;
-//import com.example.second.dao.JournalismCountDao;
 import com.example.second.dao.JournalismDao;
 import com.example.second.entity.Journalism;
-//import com.example.second.entity.JournalismCount;
+import com.example.second.entity.Teacher;
+import com.example.second.exception.ValidateException;
 import com.example.second.service.JournalismService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.List;
 
 
 @Service
-//@Transactional(rollbackFor = Exception.class)
+
 public class JournalismServiceBean implements JournalismService {
 
     @Autowired
     private JournalismDao journalismDao;
 
-//    @Autowired
-//    private JournalismCountDao journalismCountDao;
-
-//    @Autowired
-//    JournalismCopyDao journalismCopyDao;
-
     public List<Journalism> findAllJournalisms() {
         return this.journalismDao.findAllJournalisms();
     }
 
-
     @Transactional
     public void addJournalism(Journalism journalism) {
-        this.journalismDao.addJournalism(journalism);
-//        this.journalismCopyDao.addJournalism(journalism);
-        Integer count = this.journalismDao.getCountJournalisms();
-        //没有加@Transactional注解，万一出现错误，新增人数会成功，但是学生表的总人数没有改变，出现了数据的不一致。
-//        int x = 5 / 0;
-//        this.journalismCountDao.addCount(count);
+        List<String> errors = errors(journalism);
+        if (errors.size() == 0) {
+            this.journalismDao.addJournalism(journalism);
+        } else {
+            throw new ValidateException("500", errors.toString());
+        }
     }
 
-    public void deleteJournalismById(Long uid) {
-        this.journalismDao.deleteJournalismById(uid);
-        Integer count = this.journalismDao.getCountJournalisms();
-//        this.journalismCountDao.addCount(count);
-    }
+    public void deleteJournalismById(Long uid) { this.journalismDao.deleteJournalismById(uid); }
 
     public void updateJournalism(Journalism journalism) {
-        this.journalismDao.updateJournalism(journalism);
+        List<String> errors = errors(journalism);
+        if (errors.size() == 0) {
+            this.journalismDao.updateJournalism(journalism);
+        } else {
+            throw new ValidateException("500", errors.toString());
+        }
+    }
+
+    public Journalism findJournalismByUid(Long uid) {
+        return this.journalismDao.findJournalismByUid(uid);
     }
 
     public Journalism findJournalismByTitle(String title) {
@@ -58,5 +58,22 @@ public class JournalismServiceBean implements JournalismService {
     public Integer deleteJournalismsByIds(Integer[] ids) {
         return this.journalismDao.deleteJournalismsByIds(ids);
     }
+    public List<String> errors(Journalism journalism) {
+        List<String> errors = new ArrayList<>();
 
+        if (journalism.gettitle() == null || journalism.gettitle().equals(""))
+            errors.add("标题不能为空");
+
+        if (journalism.getcontent() == null || journalism.getcontent().equals(""))
+            errors.add("内容不能为空");
+
+        if (journalism.getAuthor() == null || journalism.getAuthor().equals(""))
+            errors.add("作者不能为空");
+        if (journalism.getAuthor().length() < 2 || journalism.getAuthor().length() > 20)
+            errors.add("作者长度为2到20个字符");
+        if (!journalism.getAuthor().matches("^[\\u4e00-\\u9fa5]+$"))
+            errors.add("作者必须是中文");
+
+        return errors;
+    }
 }
